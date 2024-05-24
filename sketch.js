f39fc2b6e87e358dff25880aa261ecc3d1a00742
GeorgeWBush758 <161519487+GeorgeWBush758@users.noreply.github.com>
2024-05-24 13:05:18 -0600

    13446

--- sketch.js
+++ sketch.js
@@ -1,18 +1,28 @@
 //8080
-
-let state = "start";
-let radius = 50;
-
 let x;
 let y; 
 let dx;
 let dy;
+let radius = 50;
+let state = "start";
+
+
+
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   text("hello", 50,50);
+
+
+  x = width / 2;
+  y = height / 2;
+  dx = 5;
+  dy = 5;
+
+
+
 }
 
 function draw() {
@@ -22,23 +32,41 @@ function draw() {
 
   
   if (state === "start") {
-    fill("red");
-    text("hello", width/2, height/2);
-    background("black");
-    textSize(50);
-    textAlign(CENTER);
+    showInstructions();
+    
 
   }
-  if (state === "game") {
-    background("white");
+  else if (state === "game") {
+    background("cyan");
 
 
     displayPlatform();
-    displayBall();
     displayBricks();
+    drawCircle();
+    moveCircle();
+    bounceOffWall();
 
   }
 }
+
+function showInstructions() {
+  background("black");
+  fill("red");
+  textSize(50);
+  text("Breakout", width/2, height/2);
+  text("click to begin", width/2, height/1.70);
+  
+  
+  
+  textAlign(CENTER);
+
+}
+
+
+
+
+
+
 
 function mousePressed() {
   if (state ==="start" && mousePressed) {
@@ -46,22 +74,48 @@ function mousePressed() {
   }
 }
 
+function drawCircle() {
+  //display circle
+  fill("red");
+  circle(x, y, 30);
+}
 
+function moveCircle() {
+  //move circle
+  x += dx;
+  y += dy;
+}
 
-
+function bounceOffWall() {
+  //bounce if needed
+  if (x + radius >= width || x - radius <= 0) {
+    dx = -1 * dx;
+    
+    console.log("bounced!");
+  }
+  if (y + radius >= height || y - radius <= 0) {
+    dy = -1 * dy;
+  
+    console.log("bounced!");
+  }
+}
 
 
  
 
 function displayPlatform() {
-  rect(mouseX, 700, 130, 15);
+  rect(mouseX, 700, 180, 15);
   
 }
 
-function displayBall() {
-  circle(width/2, height/2, 50);
 
-}
+
+
+
+
+
+
+
 
 function displayBricks() {
   rect(10, 50, 150, 45);
@@ -81,6 +135,8 @@ function displayBricks() {
   rect(900, 110, 150, 45); 
   rect(1100, 110, 150, 45); 
   rect(1300, 110, 150, 45);
+  rect(1500, 110, 50, 45);
+
 
   //third row 
   rect(10, 170, 150, 45);
@@ -91,6 +147,16 @@ function displayBricks() {
   rect(1000, 170, 150, 45);
   rect(1200, 170, 150, 45);
   rect(1400, 170, 150, 45);
+  // fourth row
+  rect(10, 230, 50, 45);
+  rect(100, 230, 150, 45);
+  rect(300, 230, 150, 45);
+  rect(500, 230, 150, 45);
+  rect(700, 230, 150, 45);
+  rect(900, 230, 150, 45);
+  rect(1100, 230, 150, 45);
+  rect(1300, 230, 150, 45);
+  rect(1500, 230, 50, 45);
 
 
 
@@ -100,6 +166,8 @@ function displayBricks() {
 // class Brick 
 
 // make array for brick 
+
+// add the math i ++ stuff
 
 
 
